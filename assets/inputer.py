c12441cd7f6bcd1cca97303dfeaa5e1978aaa909
Tamino <roidingertamino@gmail.com>
2025-03-07 20:55:24 +0100

    Update inputer.py

--- assets/inputer.py
+++ assets/inputer.py
@@ -16,13 +16,24 @@ def safe_input(msg: str = "default", data_type = str):
             continue #- Only allowed inputs
 
 def menue(): #- Menu to print
-    avbl = ["1", "2", "3", "4", "5", "6"]
+    avbl = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"]
+    print()
+    print("| LocalHDS Build")
     print("1) Change Local Domain URL")
     print("2) Add Local Domain overwrite")
     print("3) Remove Local Domain overwrite")
     print("4) List Local Domains")
-    print("5) Log History")
+    print()
+    print("| Log Build")
+    print("5) Show Log History")
     print("6) Save Log History")
+    print()
+    print("| Website Build")
+    print("7) New Page (in work)")
+    print("8) Remove Page (in work)")
+    print("9) Show all Pages (in work)")
+    print("10) Generate File")
+    print("11) Start Last Generated File")
 
     while True:
         choice = safe_input(f"Enter you choice ({avbl[0]}-{avbl[-1]}): ")
@@ -31,3 +42,37 @@ def menue(): #- Menu to print
             continue
         else:
             return choice
+
+def show_all_sites(existing_sites_list: list):
+    print()
+    print("All Sites:")
+    for idx, site in enumerate(existing_sites_list):
+        print(f"{idx+1}. {site}")
+    print()
+
+def user_add_url_path(existing_sites_list: list):
+    while True:
+        url_path = safe_input("Add a new url Path <leave it empty for homepage> (cancel): ")
+        url_path = "/" + url_path
+        if url_path in existing_sites_list:
+            print("This site is already used! Choose another one!")
+            continue
+        if " " in url_path or "." in url_path:
+            print("You cannot use \" \" or \".\" !")
+        elif url_path.lower() == "/cancel":
+            return 0
+        existing_sites_list.append(url_path)
+        return 0
+
+def user_remove_url_path(existing_sites_list: list):
+    show_all_sites(existing_sites_list)
+    while True:
+        url_path = safe_input("Which site do you want to remove? <leave it empty for homepage>: ")
+        url_path = "/" + url_path
+        if not url_path in existing_sites_list:
+            print("This site is not in use!")
+            continue
+        elif url_path.lower() == "/cancel":
+            return 0
+        existing_sites_list.remove(url_path)
+        return 0
